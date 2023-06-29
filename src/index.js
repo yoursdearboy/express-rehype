9230feba68736cb53796f8c7a9f8b28da48516f3
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2023-06-29 16:03:48 +0000

    Added include, layout, replace directives

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import rehypeParse from "rehype-parse";
 import rehypeStringify from "rehype-stringify";
+import { readSync } from "to-vfile";
 import { unified } from "unified";
 import { visit } from "unist-util-visit";
+
+export const base = unified()
+    .use(rehypeParse, { fragment: true })
+    .use(rehypeStringify);
+
+const eqProperty = (prop, value) => node => node.properties && node.properties[prop] === value;
+const hasProperty = prop => node => node.properties && prop in node.properties;
+
+/*
+ * Merge nodes by id.
+ */
+function merge (b) {
+    return tree => {
+        visit(tree, eqProperty("id", b.properties.id), a => {
+            a.properties = Object.assign({}, a.properties, b.properties);
+            a.children = b.children;
+        });
+    }
+}
+
+export function include() {
+    return tree => {
+        visit(tree, hasProperty("re:include"), node => {
+            const path = node.properties["re:include"];
+            const part = readSync(path);
+            const res = this.parse(part);
+            node.children = res.children;
+        });
+    }
+}
 
 export const input = options => tree => {
     visit(tree, { tagName: "input" }, node => {
@@ -11,6 +42,25 @@ export const input = options => tree => {
     })
 };
 
-export const base = unified()
-    .use(rehypeParse)
-    .use(rehypeStringify);
+export function layout() {
+    return tree => {
+        visit(tree, hasProperty("re:layout"), (node, _, parent) => {
+            const path = node.properties["re:layout"];
+            const layout = readSync(path);
+            const proc = this().use(merge, node);
+            const res = proc.runSync(proc.parse(layout));
+            parent.children = res.children;
+        });
+    }
+}
+
+export function replace() {
+    return tree => {
+        visit(tree, hasProperty("re:replace"), (node, _, parent) => {
+            const path = node.properties["re:replace"];
+            const part = readSync(path);
+            const res = this.parse(part);
+            parent.children[parent.children.indexOf(node)] = res;
+        });
+    }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
 import { read } from "to-vfile";
-import { base, input } from "./index";
+import { base, include, input, layout, replace } from "./index";
 
 test("works", async () => {
     const data = {
         key1: "value1"
     };
-    const file = await read("demo.html");
-    const res = base.use(input, data).process(file);
+    const file = await read("views/demo.html");
+    const res = await base.use(include).use(input, data).use(layout).use(replace).process(file);
     console.log(res);
 });
