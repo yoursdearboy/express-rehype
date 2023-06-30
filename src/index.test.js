30cb81c2ad791fa8147d00854a5390bd5511ee2f
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2023-06-30 08:53:30 +0000

    Rewrited test with expect

--- src/index.test.js
+++ src/index.test.js
@@ -5,7 +5,8 @@ test("works", async () => {
     const data = {
         key1: "value1"
     };
-    const file = await read("views/demo.html");
-    const res = await base.use(include).use(input, data).use(layout).use(replace).process(file);
-    console.log(res);
+    const file = await read("src/__tests__/input.html");
+    const expected = await read("src/__tests__/output.html", "utf8");
+    const result = await base.use(include).use(input, data).use(layout).use(replace).process(file);
+    expect(result.value).toEqualIgnoringWhitespace(expected.value);
 });
