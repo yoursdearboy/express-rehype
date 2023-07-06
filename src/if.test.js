8b0d09ffbb6cccaba71e411605a1c860ce113fe6
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2023-07-06 11:47:55 +0000

    Fixed typo

--- src/if.test.js
+++ src/if.test.js
@@ -7,13 +7,13 @@ const processor = unified()
     .use(cond);
 
 test("if", async () => {
-    const _input =
+    const input =
         '<div re:if="x <= 10">fine</div>' +
         '<div re:if="x > 10">too much</div>';
     const output1 = '<div re:if="x <= 10">fine</div>';
-    const branch1 = await processor().data({ x: 7 }).process(_input);
+    const branch1 = await processor().data({ x: 7 }).process(input);
     expect(branch1.value).toEqualIgnoringWhitespace(output1);
     const output2 = '<div re:if="x > 10">too much</div>';
-    const branch2 = await processor().data({ x: 42 }).process(_input);
+    const branch2 = await processor().data({ x: 42 }).process(input);
     expect(branch2.value).toEqualIgnoringWhitespace(output2);
 });
